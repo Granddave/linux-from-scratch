60f4cbe65dbb64c9b9427cb42a7c1a57e23a6218
David Isaksson <davidisaksson93@gmail.com>
2024-09-01 18:03:20 +0200

    Add patches to handle revoked version of expat-2.6.0

--- scripts/packages/fetch.sh
+++ scripts/packages/fetch.sh
@@ -6,10 +6,12 @@ pushd $LFS/sources
 
 # Fetch the sources
 wget https://www.linuxfromscratch.org/lfs/view/$LFS_VERSION/wget-list-systemd
+patch < $LFS/scripts/packages/wget-list-systemd.patch
 wget --input-file=wget-list-systemd --continue --directory-prefix=$LFS/sources
 
 # Verify the downloaded sources
 wget https://www.linuxfromscratch.org/lfs/view/$LFS_VERSION/md5sums
+patch < $LFS/scripts/packages/md5sums.patch
 md5sum -c md5sums
 
 chown root:root $LFS/sources/*
